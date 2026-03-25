7a267fa02d9dfbe101c49a28237f40408c414da4
sabryguidoum38100-jpg <sabryguidoum38100@gmail.com>
2026-03-25 07:00:46 +0100

    Elite Production: Frontend config files

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,16 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   env: {
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8080',
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
